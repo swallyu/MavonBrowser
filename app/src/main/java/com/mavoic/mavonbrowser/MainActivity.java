4567f123000e0dfabb9d359189d2e08f9e75830e
liangzy <lzhyubai@hotmail.com>
2017-12-18 15:20:54 +0800

    Initial commit

---
+++ app/src/main/java/com/mavoic/mavonbrowser/MainActivity.java
@@ -0,0 +1,103 @@
+package com.mavoic.mavonbrowser;
+
+import android.net.http.SslError;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.gyf.barlibrary.ImmersionBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = this.findViewById(R.id.toolbar);
+
+        ImmersionBar.with(this).statusBarDarkFont(true).titleBar(findViewById(R.id.layout_toolbar)).init();
+
+        webView = this.findViewById(R.id.webview);
+        initSetting();
+        chromeClient();
+        webView.loadUrl("https://www.jd.com/");
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+    }
+
+    private void initSetting(){
+        //声明WebSettings子类
+        WebSettings webSettings = webView.getSettings();
+
+//如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
+        webSettings.setJavaScriptEnabled(true);
+// 若加载的 html 里有JS 在执行动画等操作，会造成资源浪费（CPU、电量）
+// 在 onStop 和 onResume 里分别把 setJavaScriptEnabled() 给设置成 false 和 true 即可
+
+//支持插件
+        //webSettings.setPluginsEnabled(true);
+
+//设置自适应屏幕，两者合用
+        webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+        webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+
+//缩放操作
+        webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+        webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+
+//其他细节操作
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
+        webSettings.setAllowFileAccess(true); //设置可以访问文件
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
+        webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
+        webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+
+    }
+
+    private void chromeClient() {
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress < 100) {
+                    //String progress = newProgress + "%";
+                    //progress.setText(progress);
+                } else {
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                //titleview.setText(title);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+
+    }
+}
